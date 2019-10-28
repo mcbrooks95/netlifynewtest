393cd94e2cf52aa4e6a03472f1b4d12d79a5df67
Mark Brooks <mcbrooks95@gmail.com>
2019-10-28 00:32:20 -0500

    lots of things changed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { Query, Mutation } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edivvvvt <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GET_TEST_DATA = gql`
+query GetTestTable {
+  test_table {
+    test_column
+    test_column_2
+  }
 }
+`
+const INSERT_TEST_DATA =  gql`
+mutation InsertTestTable {
+  __typename
+  insert_test_table(objects: {test_column: "vvvcx555555cvv", test_column_2: 533}) {
+    returning {
+      test_column
+      test_column_2
+    }
+  }
+}`
+
+
+  const App = () => (
+  <Query query={GET_TEST_DATA}>
+    {({ loading, error, data }) => {
+      
+      if (loading) return <div>Loading...</div>;
+      if (error) return <div>Error :(</div>;
+        
+        console.log(`about to print data first time = ` + data)
+        let getData = data
+      return (
+        
+        <Mutation mutation={INSERT_TEST_DATA}
+        refetchQueries={
+          [{ query: GET_TEST_DATA }]
+        }
+        >
+        
+        {(INSERT_TEST_DATA, { loading, error, data }) => {
+          console.log(`about to print getData = ` + getData)
+          return(
+                          <div className="App">
+                            <header className="App-header">
+                              <img src={logo} className="App-logo" alt="logo" />
+                              <p>
+                                Edivvvvt <code>src/App.js</code> and save to reload.
+                              </p>
+                                {getData.test_table.map(element => {
+                                  console.log(`element is ` + element)
+                                  console.log(element)
+                                  console.log(element.test_column)
+                                  // <p>as44df</p>;
+                                  // return (<p>asdf</p>)
+                                  return (<p>{element.test_column} {element.test_column_2}</p>)
+                                  })}
+                                
+                              <a
+                                className="App-link"
+                                href="https://reactjs.org"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                Learn React
+                              </a>
+
+                              <a
+                                  onClick={() => {
+                                    INSERT_TEST_DATA()
+                                  }
+                                  }
+                              >
+                                Mutation call
+                              </a>
+                            </header>
+                          </div>
+          )
+        }}
+        </Mutation>
+      );
+    }}
+  </Query>
+// }
+  )
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React from 'react'
+import { withRouter, Router } from 'react-router'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import ApolloProvider from 'apollo-client'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+// const defaultOptions = {
+//     watchQuery: {
+//       fetchPolicy: 'cache-and-network',
+//       errorPolicy: 'ignore',
+//     },
+//     query: {
+//       fetchPolicy: 'network-only',
+//       errorPolicy: 'all',
+//     },
+//     mutate: {
+//       errorPolicy: 'all'
+//     }
+//   }
+
+// const cache = new InMemoryCache();
+// const client = new ApolloClient({
+//     defaultOptions,
+//   cache,
+//   link: new HttpLink({
+//     uri: 'https://asdfasdff444.herokuapp.com/v1/graphql',
+//   })
+// });
+
+
+//   ReactDOM.render(
+//     <ApolloProvider client={client}>
+//       <Router >
+//         <App />
+//       </Router>
+//     </ApolloProvider>,
+//     document.getElementById('root'),
+//   )
+
+
+
+
+
+
+
+ 
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({ uri: 'https://asdfasdff444.herokuapp.com/v1/graphql' });
+ 
+const ApolloApp = AppComponent => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+ 
+render(ApolloApp(App), document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
